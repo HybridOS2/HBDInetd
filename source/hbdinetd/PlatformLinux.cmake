5c676cdd7974ee7ea15e113928a941bc78ea89f0
Vincent Wei <vincent@minigui.org>
2023-05-17 13:10:40 +0800

    rename wifi-device.c to wifi-ops.c

--- source/hbdinetd/PlatformLinux.cmake
+++ source/hbdinetd/PlatformLinux.cmake
@@ -16,7 +16,7 @@ list(APPEND hbdinetd_SOURCES
     "${HBDINETD_DIR}/port/linux/wpa-client/os_unix.c"
     "${HBDINETD_DIR}/port/linux/wpa-client/wpa_debug.c"
     "${HBDINETD_DIR}/port/linux/wpa-client/wpa_ctrl.c"
-    "${HBDINETD_DIR}/port/linux/wifi/wifi-device.c"
+    "${HBDINETD_DIR}/port/linux/wifi/wifi-ops.c"
     "${HBDINETD_DIR}/port/linux/wifi/wifi.c"
     "${HBDINETD_DIR}/port/linux/wifi/wpa_supplicant_conf.c"
 #    "${HBDINETD_DIR}/port/linux/wifi/wifi_event.c"
